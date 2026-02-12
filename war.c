894e08a428ebb049c680e13e08c5574a32fbf20a
Guilherme Lino <guilhermelino@msn.com>
2026-02-12 17:58:42 -0300

    nivel intermediario
    com mapa top

--- war.c
+++ war.c
@@ -1,58 +1,148 @@
+//nivel intermediario
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-
-//CADASTRA O LIMITE DE CARACTERES
-
-
-struct territorio
+struct territorio // Struct do território
 {
     char nome[30];
     char cor[10];
-    int tropas;
+    int *tropas; //aqui no * que vamos usar no calloc depois
 };
 
-int main(){
-       
-    printf("\n ------------------------------------------");
-    
-    struct territorio territorios[5]; 
+//agora as funções que vamos usar  dentro do main, os ponteiros -> substituiram o monte de informaçao que tinhamos antes
 
-    //LAÇO DE CADASTROS 5X
-    for (int i=0; i<5; i++)
-    {
-        printf("\nCadastro de territórios:%d\n", i + 1);//+1 PRA COMEÇAR DO 1 NÃO DO ZERO
+void cadastrar_territorios(struct territorio *terri) {
+
+    for (int i = 0; i < 5; i++) {
+        printf("\nCadastro de território %d\n", i + 1);
 
         printf("Nome: ");
-        scanf(" %29[^\n]", territorios[i].nome); // ESSE CODIGO NO COMEÇO PERMITE USAR ESPAÇO ENTRE AS PALAVRAS, SE NÃO ELE DA O ENTER E PULA UM CAMPO
+        scanf("%s", terri[i].nome);
 
         printf("Cor: ");
-        scanf(" %9[^\n]", territorios[i].cor);   // MESMA COISA, PODE USAR 2 NOMES P/ COR
+        scanf("%s", terri[i].cor);
+
+        // alocando a tropa com calloc
+        terri[i].tropas = calloc(1, sizeof(int));
 
         printf("Numero de tropas: ");
-        scanf("%d", &territorios[i].tropas); // 
+        scanf("%d", terri[i].tropas); // aqui é sem & pq já é ponteiro
+    }
+}
 
-        printf("\n =============================================");
 
-        
+//achei esse desenho na web, adorei o resultado (sei que aumentou o codigo, mas o visual ficou muito bom)
+void exibir_mapa(struct territorio territorios[5]) {
+
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣄⣠⣀⡀⣀⣠⣤⣤⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⢠⣠⣼⣿⣿⣿⣟⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⢠⣤⣦⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+printf("⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣟⣾⣿⣽⣿⣿⣅⠈⠉⠻⣿⣿⣿⣿⣿⡿⠇⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⢀⡶⠒⢉⡀⢠⣤⣶⣶⣿⣷⣆⣀⡀⠀⢲⣖⠒⠀⠀⠀⠀⠀⠀⠀\n");
+printf("⢀⣤⣾⣶⣦⣤⣤⣶⣿⣿⣿⣿⣿⣿⣽⡿⠻⣷⣀⠀⢻⣿⣿⣿⡿⠟⠀⠀⠀⠀⠀⠀⣤⣶⣶⣤⣀⣀⣬⣷⣦⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣤⣦⣼⣀⠀\n");
+printf("⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠓⣿⣿⠟⠁⠘⣿⡟⠁⠀⠘⠛⠁⠀⠀⢠⣾⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠏⠙⠁\n");
+printf("⠀⠸⠟⠋⠀⠈⠙⣿⣿⣿⣿⣿⣿⣷⣦⡄⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⣼⣆⢘⣿⣯⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡉⠉⢱⡿⠀⠀⠀⠀⠀\n");
+printf("⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⡿⠦⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⡗⠀⠈⠀⠀⠀⠀⠀⠀\n");
+printf("⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣉⣿⡿⢿⢷⣾⣾⣿⣞⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⣠⠟⠀⠀⠀⠀⠀⠀⠀⠀\n");
+printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⠿⠿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣾⣿⣿⣷⣦⣶⣦⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠈⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣤⡖⠛⠶⠤⡀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠙⣿⣿⠿⢻⣿⣿⡿⠋⢩⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠧⣤⣦⣤⣄⡀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠘⣧⠀⠈⣹⡻⠇⢀⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣤⣀⡀⠀⠀⠀⠀⠀⠀⠈⢽⣿⣿⣿⣿⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠹⣷⣴⣿⣷⢲⣦⣤⡀⢀⡀⠀⠀⠀⠀⠀⠀\n");
+printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣷⢀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠂⠛⣆⣤⡜⣟⠋⠙⠂⠀⠀⠀⠀⠀\n");
+printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⠉⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣾⣿⣿⣿⣿⣆⠀⠰⠄⠀⠉⠀⠀\n");
+printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⠿⠿⣿⣿⣿⠇⠀⠀⢀⠀⠀⠀\n");
+printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⡿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⡇⠀⠀⢀⣼⠗⠀⠀\n");
+printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠃⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠁⠀⠀⠀\n");
+printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+
+    printf("\n====================================================\n");
+    printf("\n|> > > > > > >> > MAPA MUNDI WAR < < < < < < < < < |\n");
+    printf("\n====================++++++++++======================\n");
+
+    for (int i = 0; i < 5; i++) {
+        printf(" %d .  %s   |>>  Exército %s    |>> Tropas: %d\n",
+            i + 1,
+            territorios[i].nome,
+            territorios[i].cor,
+            *territorios[i].tropas // aqui tem que usar * por causa do calloc
+        );
+    }
+}
+
+void batalha(struct territorio territorios[5]) {
+
+    char continuar[5];
+    int a, d; //as variaveis de ataque (a) e defesa (d)
+    
+    //enquanto for verdadeiro(1) - se fosse falso (0)
+    while (1) {
+
+        printf("\nQual país vai atacar (1 a 5)? ");
+        scanf("%d", &a);
+
+        printf("Quem defende (1 a 5)? ");
+        scanf("%d", &d);
+
+        // indices começam em 0
+        a--;
+        d--;
+
+        int dado1 = rand() % 6 + 1;
+        int dado2 = rand() % 6 + 1;
+
+        printf("\nDado atacante saiu: %d\n", dado1);
+        printf("Dado defensor saiu: %d\n", dado2);
+
+        if (dado1 > dado2) {
+            (*territorios[d].tropas)--;
+            if (*territorios[d].tropas < 0) *territorios[d].tropas = 0;
+            printf("Atacante venceu!\n");
+        } 
+        else if (dado2 > dado1) {
+            (*territorios[a].tropas)--;
+            if (*territorios[a].tropas < 0) *territorios[a].tropas = 0;
+            printf("Defensor venceu!\n");
+        } 
+        else {
+            printf("Empate\n");
+        }
+
+        printf("\nPara continuar a guerra pressione ENTER...\n");
+        getchar();   // limpa ENTER que sobrou do scanf
+        fgets(continuar, sizeof(continuar), stdin);
+
+        // se digitar só ENTER, continua
+        if (continuar[0] == '\n') {
+            exibir_mapa(territorios); 
+            continue;
+        }
+
+        // se digitar qualquer outra coisa, sai do loop
+        break;
+    }
+}
+
+
+int main() {
+
+    srand(time(NULL)); // funcao random
+
+    struct territorio territorios[5];
+
+    cadastrar_territorios(territorios);
+
+    exibir_mapa(territorios);
+
+    batalha(territorios);
+
+
+    exibir_mapa(territorios);
+
+    //essa parte vai ser um loop que vai liberar o calloc em cada uma das tropas que foram alocadas
+    for (int i = 0; i < 5; i++) {
+        free(territorios[i].tropas);
     }
 
-    printf("\n \n Territorios Cadastrados: \n \n"); //MOSTRA O QUE  EU CADASTRAR NESSE LOOP
-
-    for (int i=0; i<5; i++)
-    {
-        printf("\n =============================================");
-        
-        printf("\nTerritório Nº  %d\n", i+1); 
-
-        printf("Nome: %s\n", territorios[i].nome);
-
-        printf("Cor: %s\n", territorios[i].cor);
- 
-        printf("Numero de tropas: %d\n", territorios[i].tropas);
-
-    }
-        
     return 0;
 }

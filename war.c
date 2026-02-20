689e70717a1b248078148f15a62bbd68da6c40a2
Guilherme Lino <guilhermelino@msn.com>
2026-02-20 14:37:30 -0300

    nivel mestre

--- war.c
+++ war.c
@@ -1,22 +1,38 @@
-//nivel intermediario
+//nivel mestre
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-struct territorio // Struct do território
+// validação de numeros inteiros
+int ler_inteiro() {
+    int num;
+    char c;
+    while (1) {
+        if (scanf("%d", &num) == 1) {  
+            return num;
+        }
+        else {
+            printf("Entrada inválida! Digite um número válido: ");
+            while ((c = getchar()) != '\n' && c != EOF);
+        }
+    }
+}
+
+//struct do territorio
+struct territorio 
 {
     char nome[30];
     char cor[10];
     int *tropas; //aqui no * que vamos usar no calloc depois
 };
 
-//agora as funções que vamos usar  dentro do main, os ponteiros -> substituiram o monte de informaçao que tinhamos antes
-
+//cadastramento de territorios, tropas tem que ter mais de 1 se náo ele nao deixa prosseguir
 void cadastrar_territorios(struct territorio *terri) {
 
     for (int i = 0; i < 5; i++) {
+
         printf("\nCadastro de território %d\n", i + 1);
 
         printf("Nome: ");
@@ -28,40 +44,47 @@ void cadastrar_territorios(struct territorio *terri) {
         // alocando a tropa com calloc
         terri[i].tropas = calloc(1, sizeof(int));
 
-        printf("Numero de tropas: ");
-        scanf("%d", terri[i].tropas); // aqui é sem & pq já é ponteiro
+        // validação pra nao dar erro se digitar letras
+        do {
+            printf("Numero de tropas (min 1): ");
+            *terri[i].tropas = ler_inteiro();
+
+            if (*terri[i].tropas < 1)
+                printf("Valor inválido! Tropas devem ser >= 1.\n");
+
+        } while (*terri[i].tropas < 1);
     }
 }
 
-
-//achei esse desenho na web, adorei o resultado (sei que aumentou o codigo, mas o visual ficou muito bom)
+//mapa mundi em ascii pra dar um visual legal
 void exibir_mapa(struct territorio territorios[5]) {
 
+    // --- sua arte inteira mantida ---
     printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣄⣠⣀⡀⣀⣠⣤⣤⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
-printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⢠⣠⣼⣿⣿⣿⣟⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⢠⣤⣦⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
-printf("⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣟⣾⣿⣽⣿⣿⣅⠈⠉⠻⣿⣿⣿⣿⣿⡿⠇⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⢀⡶⠒⢉⡀⢠⣤⣶⣶⣿⣷⣆⣀⡀⠀⢲⣖⠒⠀⠀⠀⠀⠀⠀⠀\n");
-printf("⢀⣤⣾⣶⣦⣤⣤⣶⣿⣿⣿⣿⣿⣿⣽⡿⠻⣷⣀⠀⢻⣿⣿⣿⡿⠟⠀⠀⠀⠀⠀⠀⣤⣶⣶⣤⣀⣀⣬⣷⣦⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣤⣦⣼⣀⠀\n");
-printf("⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠓⣿⣿⠟⠁⠘⣿⡟⠁⠀⠘⠛⠁⠀⠀⢠⣾⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠏⠙⠁\n");
-printf("⠀⠸⠟⠋⠀⠈⠙⣿⣿⣿⣿⣿⣿⣷⣦⡄⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⣼⣆⢘⣿⣯⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡉⠉⢱⡿⠀⠀⠀⠀⠀\n");
-printf("⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⡿⠦⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⡗⠀⠈⠀⠀⠀⠀⠀⠀\n");
-printf("⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣉⣿⡿⢿⢷⣾⣾⣿⣞⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⣠⠟⠀⠀⠀⠀⠀⠀⠀⠀\n");
-printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⠿⠿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣾⣿⣿⣷⣦⣶⣦⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠈⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
-printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣤⡖⠛⠶⠤⡀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠙⣿⣿⠿⢻⣿⣿⡿⠋⢩⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
-printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠧⣤⣦⣤⣄⡀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠘⣧⠀⠈⣹⡻⠇⢀⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
-printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣤⣀⡀⠀⠀⠀⠀⠀⠀⠈⢽⣿⣿⣿⣿⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠹⣷⣴⣿⣷⢲⣦⣤⡀⢀⡀⠀⠀⠀⠀⠀⠀\n");
-printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣷⢀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠂⠛⣆⣤⡜⣟⠋⠙⠂⠀⠀⠀⠀⠀\n");
-printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⠉⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣾⣿⣿⣿⣿⣆⠀⠰⠄⠀⠉⠀⠀\n");
-printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⠿⠿⣿⣿⣿⠇⠀⠀⢀⠀⠀⠀\n");
-printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⡿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⡇⠀⠀⢀⣼⠗⠀⠀\n");
-printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠃⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠁⠀⠀⠀\n");
-printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⢠⣠⣼⣿⣿⣿⣟⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⢠⣤⣦⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣟⣾⣿⣽⣿⣿⣅⠈⠉⠻⣿⣿⣿⣿⣿⡿⠇⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⢀⡶⠒⢉⡀⢠⣤⣶⣶⣿⣷⣆⣀⡀⠀⢲⣖⠒⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⢀⣤⣾⣶⣦⣤⣤⣶⣿⣿⣿⣿⣿⣿⣽⡿⠻⣷⣀⠀⢻⣿⣿⣿⡿⠟⠀⠀⠀⠀⠀⠀⣤⣶⣶⣤⣀⣀⣬⣷⣦⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣤⣦⣼⣀⠀\n");
+    printf("⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠓⣿⣿⠟⠁⠘⣿⡟⠁⠀⠘⠛⠁⠀⠀⢠⣾⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠏⠙⠁\n");
+    printf("⠀⠸⠟⠋⠀⠈⠙⣿⣿⣿⣿⣿⣿⣷⣦⡄⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⣼⣆⢘⣿⣯⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡉⠉⢱⡿⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⡿⠦⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⡗⠀⠈⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣉⣿⡿⢿⢷⣾⣾⣿⣞⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⣠⠟⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⠿⠿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣾⣿⣿⣷⣦⣶⣦⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠈⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣤⡖⠛⠶⠤⡀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠙⣿⣿⠿⢻⣿⣿⡿⠋⢩⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠧⣤⣦⣤⣄⡀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠘⣧⠀⠈⣹⡻⠇⢀⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣤⣀⡀⠀⠀⠀⠀⠀⠀⠈⢽⣿⣿⣿⣿⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠹⣷⣴⣿⣷⢲⣦⣤⡀⢀⡀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣷⢀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠂⠛⣆⣤⡜⣟⠋⠙⠂⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⠉⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣾⣿⣿⣿⣿⣆⠀⠰⠄⠀⠉⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⠿⠿⣿⣿⣿⠇⠀⠀⢀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⡿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⡇⠀⠀⢀⣼⠗⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠃⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠁⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
 
-    printf("\n====================================================\n");
-    printf("\n|> > > > > > >> > MAPA MUNDI WAR < < < < < < < < < |\n");
-    printf("\n====================++++++++++======================\n");
+    printf("\n    ====================================================    \n");
+    printf("\n    |> > > > > > >> > MAPA MUNDI WAR < < < < < < < < < |    \n");
+    printf("\n    ====================++++++++++======================    \n");
 
     for (int i = 0; i < 5; i++) {
-        printf(" %d .  %s   |>>  Exército %s    |>> Tropas: %d\n",
+        printf(" %d .%s    |>>  Exército %s    |>> Tropas: %d\n",
             i + 1,
             territorios[i].nome,
             territorios[i].cor,
@@ -70,79 +93,182 @@ printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠒⠀⠀⠀⠀⠀
     }
 }
 
-void batalha(struct territorio territorios[5]) {
+//função que mostra o placar quando a pessoa seleciona o menu depois do cadastro
+void mostrar_placar(struct territorio t[5]) {
+    printf("\n========== PLACAR ==========\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%s (%s) -> %d tropas\n",
+               t[i].nome, t[i].cor, *t[i].tropas);
+    }
+    printf("===========================\n");
+}
 
-    char continuar[5];
-    int a, d; //as variaveis de ataque (a) e defesa (d)
-    
-    //enquanto for verdadeiro(1) - se fosse falso (0)
+//aqui é onde mostramos a missão sorteada(bem simples mesmo)
+void mostrar_missao(int tipo, char cor_alvo[10]) {
+
+    printf("\n========== MISSÃO ==========\n");
+
+    if (tipo == 1) {
+        printf("Missão: Eliminar a cor: %s\n", cor_alvo);
+    }
+    else {
+        printf("Missão: Eliminar 2 países (2 territórios zerados)\n");
+    }
+
+    printf("===========================\n");
+}
+//validando se a missão foi cumprida
+int checar_missao(struct territorio t[5], int tipo, char cor_alvo[10]) {
+
+    if (tipo == 1) {
+        // missão 1: eliminar uma cor
+        for (int i = 0; i < 5; i++) {
+            if (strcmp(t[i].cor, cor_alvo) == 0) {
+                if (*t[i].tropas > 0)
+                    return 0;  // ainda não eliminou a cor
+            }
+        }
+        return 1;  // eliminação concluída
+    }
+    else {
+        // missão 2: zerar 2 territórios
+        int mortos = 0;
+        for (int i = 0; i < 5; i++) {
+            if (*t[i].tropas == 0)
+                mortos++;
+        }
+        if (mortos >= 2) return 1;
+        return 0;
+    }
+}
+
+//funcao da batalha, com validação se ataca exercito zerado ou a si mesmo
+void batalha(struct territorio territorios[5], int tipo_missao, char cor_alvo[10]) {
+
+    int a, d; 
+    char pausa;
+
     while (1) {
 
         printf("\nQual país vai atacar (1 a 5)? ");
-        scanf("%d", &a);
+        a = ler_inteiro();
 
         printf("Quem defende (1 a 5)? ");
-        scanf("%d", &d);
+        d = ler_inteiro();
 
-        // indices começam em 0
-        a--;
-        d--;
+        // validacoes, a seleção nao pode sair da faixa de paises cadastrados entre 1 e 5 apenas
+        if (a < 1 || a > 5 || d < 1 || d > 5) {
+            printf("Numero inválido!\n");
+            continue;
+        }
+
+        if (a == d) {
+            printf("Um país não pode atacar ele mesmo!\n");
+            continue;
+        }
+
+        if (*territorios[a-1].tropas == 0) {
+            printf("O atacante está destruído!\n");
+            continue;
+        }
+
+        if (*territorios[d-1].tropas == 0) {
+            printf("O defensor já está destruído!\n");
+            continue;
+        }
+
+        a--; d--;
 
         int dado1 = rand() % 6 + 1;
         int dado2 = rand() % 6 + 1;
 
-        printf("\nDado atacante saiu: %d\n", dado1);
-        printf("Dado defensor saiu: %d\n", dado2);
+        printf("\nDado atacante: %d\n", dado1);
+        printf("Dado defensor: %d\n", dado2);
 
         if (dado1 > dado2) {
             (*territorios[d].tropas)--;
-            if (*territorios[d].tropas < 0) *territorios[d].tropas = 0;
+            if (*territorios[d].tropas < 0) 
+                *territorios[d].tropas = 0;
             printf("Atacante venceu!\n");
         } 
         else if (dado2 > dado1) {
             (*territorios[a].tropas)--;
-            if (*territorios[a].tropas < 0) *territorios[a].tropas = 0;
+            if (*territorios[a].tropas < 0) 
+                *territorios[a].tropas = 0;
             printf("Defensor venceu!\n");
         } 
         else {
             printf("Empate\n");
         }
 
-        printf("\nPara continuar a guerra pressione ENTER...\n");
-        getchar();   // limpa ENTER que sobrou do scanf
-        fgets(continuar, sizeof(continuar), stdin);
+        // mostrar mapa após ataque
+        exibir_mapa(territorios);
 
-        // se digitar só ENTER, continua
-        if (continuar[0] == '\n') {
-            exibir_mapa(territorios); 
-            continue;
+        // checar missão
+        if (checar_missao(territorios, tipo_missao, cor_alvo)) {
+            printf("\nPARABÉNS !!! MISSÃO CUMPRIDA!!!\n");
         }
 
-        // se digitar qualquer outra coisa, sai do loop
-        break;
+        printf("\nPressione ENTER para voltar ao menu...");
+        while ((pausa = getchar()) != '\n');
+        return; // volta ao menu
     }
 }
 
-
+//Main com as funçoes para rodar o codigo
 int main() {
 
-    srand(time(NULL)); // funcao random
+    srand(time(NULL));
 
     struct territorio territorios[5];
 
+    // cadastro inicial
     cadastrar_territorios(territorios);
 
     exibir_mapa(territorios);
+    
+    //gerar a missao
+    int tipo_missao = (rand() % 2) + 1; // 1 ou 2
+    char cor_alvo[10];
 
-    batalha(territorios);
-
-
-    exibir_mapa(territorios);
-
-    //essa parte vai ser um loop que vai liberar o calloc em cada uma das tropas que foram alocadas
-    for (int i = 0; i < 5; i++) {
-        free(territorios[i].tropas);
+    if (tipo_missao == 1) {
+        int sorteio = rand() % 5;
+        strcpy(cor_alvo, territorios[sorteio].cor);
     }
+    //menu com as opcoes
+    int opcao = 0;
+
+    while (opcao != 4) {
+
+        printf("\n=========== MENU ===========\n");
+        printf("1 - Atacar\n");
+        printf("2 - Mostrar Missão\n");
+        printf("3 - Mostrar Placar\n");
+        printf("4 - Sair\n");
+        printf("============================\n");
+        printf("Escolha: ");
+
+        opcao = ler_inteiro(); //validando os dados inseridos novamente
+
+        if (opcao == 1)
+            batalha(territorios, tipo_missao, cor_alvo);
+
+        else if (opcao == 2)
+            mostrar_missao(tipo_missao, cor_alvo);
+
+        else if (opcao == 3)
+            mostrar_placar(territorios);
+
+        else if (opcao == 4)
+            printf("Saindo...\n");
+
+        else
+            printf("Opção inválida!\n");
+    }
+
+    // liberar memoria das tropas do caloc
+    for (int i = 0; i < 5; i++)
+        free(territorios[i].tropas);
 
     return 0;
 }
